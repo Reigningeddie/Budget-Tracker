5cb312ea4bbfa94b471ae7298a5583aeb626fa2f
Eduardo Aguilar <eaguilar92105@yahoo.com>
2020-08-07 23:36:18 -0700

    added save record and check database functions

--- Develop/public/db.js
+++ Develop/public/db.js
@@ -15,7 +15,7 @@ request.onsuccess = function(event) {
 };
 
 request.onerror = function(event) {
-    console.log("Whoops! " + event.target.errorCode);
+    console.log("error", event);
 };
 
 function saveRecord(record) {
@@ -25,5 +25,33 @@ function saveRecord(record) {
 
     const store = transaction.objectStore("pending");
 
-    store.add(record);
+    store.add(record, {key});
 }
+
+function checkDatabase() {
+    const db = event.target.result
+    const transaction = db.transaction(["pendig"], "readwrite");
+    const pendingStore = transaction.objectStore("pending");
+    const getAll = pendingStore.getAll();
+
+    getAll.onsuccess = function() {
+        if (getAll.result.length > 0) {
+            fetch("/api/transaction/bulk", {
+                method: "POST",
+                body: JSON.stringify(getAll.result),
+                headers: {
+                    Accept: "application/json, text/plain, */*",
+                    "Content-Type": "application/json"
+                }
+            })
+            .then(response => response.json())
+            .then(() => {
+                const clearTransaction = db.transaction(["pending"], "readwrite");
+                const pendingStore =
+                const clearAll =
+            });
+        }
+    };
+}
+
+window.addEventListener("online", checkDatabase);
